781f83d749bf9b65f75e9a5353c80469677e3fa2
Retro_Guy <retro.guy@rocksolidbbs.com>
2024-05-11 04:05:56 -0700

    Oops, don't exit completely if memcache is disabled.

--- Rocksolid_Light/rslight/memcached.inc.php
+++ Rocksolid_Light/rslight/memcached.inc.php
@@ -15,19 +15,17 @@ $enable_memcache_logging = false;
 $memcache_ttl = 14400;
 
 /* PLEASE DO NOT EDIT BELOW THIS LINE */
-if(!$enable_memcache) {
-    exit;
-}
+if ($enable_memcache) {
 
-// Initiate a new object of memcache
-$memcacheD = new Memcached();
- 
-// Add server
-if ($memcacheD->addServer($memcache_server, $memcache_port)) {
-    if($enable_memcache_logging) {
-    file_put_contents($logdir . '/memcache.log', "\n".format_log_date() . ' Connected memcache ' .$memcache_server . ':' . $memcache_port, FILE_APPEND);
+    // Initiate a new object of memcache
+    $memcacheD = new Memcached();
+
+    // Add server
+    if ($memcacheD->addServer($memcache_server, $memcache_port)) {
+        if ($enable_memcache_logging) {
+            file_put_contents($logdir . '/memcache.log', "\n" . format_log_date() . ' Connected memcache ' . $memcache_server . ':' . $memcache_port, FILE_APPEND);
+        }
+    } else {
+        file_put_contents($logdir . '/memcache.log', "\n" . format_log_date() . ' Failed to connect memcache ' . $memcache_server . ':' . $memcache_port, FILE_APPEND);
     }
-}
-else {
-    file_put_contents($logdir . '/memcache.log', "\n".format_log_date() . ' Failed to connect memcache ' .$memcache_server . ':' . $memcache_port, FILE_APPEND);
 }
