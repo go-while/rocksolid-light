2fd21f8deb50ffe57dc01e54be221d20d2f3ce9c
Retro_Guy <retro.guy@rocksolidbbs.com>
2024-05-11 03:39:35 -0700

    Clean up memcached.inc.php (avoid dupe variables, etc.

--- Rocksolid_Light/rslight/memcached.inc.php
+++ Rocksolid_Light/rslight/memcached.inc.php
@@ -1,16 +1,13 @@
 <?php
 /* memcached and php-memcached must be installed */
 
+// Set to true to enable memcache
 $enable_memcache = false;
 
-if(!$enable_memcache) {
-    exit;
-}
-
 // Server & port details
-$server = '127.0.0.1';
-$port = 11211;
-
+$memcache_server = '127.0.0.1';
+$memcache_port = 11211;
+ 
 // Log all hits (log file may be large)
 $enable_memcache_logging = false;
 
@@ -18,15 +15,19 @@ $enable_memcache_logging = false;
 $memcache_ttl = 14400;
 
 /* PLEASE DO NOT EDIT BELOW THIS LINE */
+if(!$enable_memcache) {
+    exit;
+}
 
 // Initiate a new object of memcache
 $memcacheD = new Memcached();
-
+ 
 // Add server
-if ($memcacheD->addServer($server, $port)) {
-//    file_put_contents($logdir . '/debug.log', "\n".format_log_date() . ' Added memcache ' .$server . ':' . $port, FILE_APPEND);
+if ($memcacheD->addServer($memcache_server, $memcache_port)) {
+    if($enable_memcache_logging) {
+    file_put_contents($logdir . '/memcache.log', "\n".format_log_date() . ' Connected memcache ' .$memcache_server . ':' . $memcache_port, FILE_APPEND);
+    }
 }
 else {
-    file_put_contents($logdir . '/debug.log', "\n".format_log_date() . ' Failed to add memcache ' .$server . ':' . $port, FILE_APPEND);
+    file_put_contents($logdir . '/memcache.log', "\n".format_log_date() . ' Failed to connect memcache ' .$memcache_server . ':' . $memcache_port, FILE_APPEND);
 }
-
