289b5d15dca4bef3b523972b49b94ae2055243f7
Retro_Guy <retro.guy@rocksolidbbs.com>
2024-11-05 12:33:25 -0700

    Decode multibyte subject before displaying 'title' in group list page.

--- Rocksolid_Light/rocksolid/newsportal.php
+++ Rocksolid_Light/rocksolid/newsportal.php
@@ -820,7 +820,7 @@ function groups_show($gruppen)
                     $groupdisplay .= "(blocked user)";
                 } else {
                     $url = 'article-flat.php?id=' . $lastarticleinfo['number'] . '&group=' . urlencode($g->name) . '#' . $lastarticleinfo['number'];
-                    $groupdisplay .= '<a href="' . $url . '" title="' . $lastarticleinfo['subject'] . '">' . get_date_interval(date("D, j M Y H:i T", $lastarticleinfo['date'])) . '</a>';
+                    $groupdisplay .= '<a href="' . $url . '" title="' . mb_decode_mimeheader($lastarticleinfo['subject']) . '">' . get_date_interval(date("D, j M Y H:i T", $lastarticleinfo['date'])) . '</a>';
                     $groupdisplay .= '<br>by: ';
                     $groupdisplay .= create_name_link($lastarticleinfo['name'], $name_from);
                 }
