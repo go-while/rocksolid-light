50520e89aa4acea29c7c9f157bf272ef401103f0
go-while <go-while@users.noreply.github.com>
2025-06-12 18:41:02 +0100

    Update get_translation_keys.sh

--- Rocksolid_Light/rocksolid/lang/get_translation_keys.sh
+++ Rocksolid_Light/rocksolid/lang/get_translation_keys.sh
@@ -5,5 +5,5 @@ echo "checking translations... $FILE"
 
 while IFS="" read -r langfile; do
 	echo -e "\n# Lines: $(wc -l "$langfile")";
-	cut -d"]" -f1 "$langfile" |cut -d"[" -f1,2
+	cut -d"]" -f1 "$langfile" |cut -d"[" -f1,2|grep "^\\\$"
 done< <(ls "$FILE")
