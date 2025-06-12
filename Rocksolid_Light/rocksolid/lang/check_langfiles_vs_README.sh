a0ba912e4e715c533a05d207eef3aeee14e008c6
go-while <go-while@users.noreply.github.com>
2025-06-12 20:34:29 +0100

    Update check_langfiles_vs_README.sh

--- Rocksolid_Light/rocksolid/lang/check_langfiles_vs_README.sh
+++ Rocksolid_Light/rocksolid/lang/check_langfiles_vs_README.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ERRORS=0
+ERRORS=0, EXISTS=0;
 while IFS="" read -r langfile; do
  RES=$(grep PASS README.md | grep "$langfile");
  E=$?
@@ -9,6 +9,7 @@ while IFS="" read -r langfile; do
   let ERRORS="ERRORS+1"
  else
   test "$1" = "-verbose" && echo "$langfile ✅ PASS: found in README";
+  let EXISTS="EXISTS+1"
  fi
 done< <(ls *.lang)
-echo "$0: ERRORS: $ERRORS";
+echo "$0: ERRORS: $ERRORS | EXISTS: $EXISTS";
