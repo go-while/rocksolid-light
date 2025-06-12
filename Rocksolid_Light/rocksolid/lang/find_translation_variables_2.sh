58b4b0756658a1a6cea6f76a76a7e176a2447a57
go-while <go-while@users.noreply.github.com>
2025-06-13 00:00:32 +0100

    Update find_translation_variables_2.sh

--- Rocksolid_Light/rocksolid/lang/find_translation_variables_2.sh
+++ Rocksolid_Light/rocksolid/lang/find_translation_variables_2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ../../ -name "*.php" -not -path "./lang/*" -print0 | xargs -0 grep -o '\$text_[a-zA-Z_]*\["[^"]*"\]' | sort | uniq
+find ../../ -name "*.php" -not -path "./lang/*" -print0 | xargs -0 grep -o '\$text_[a-zA-Z_]*\["[^"]*"\]' |cut -d: -f2-|sort|uniq
