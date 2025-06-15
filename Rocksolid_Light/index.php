058a1a1b27118ae0c472b16f4afbf2d0a66cffc2
go-while <go-while@users.noreply.github.com>
2025-06-15 01:11:13 +0100

    Update index.php

--- Rocksolid_Light/index.php
+++ Rocksolid_Light/index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$_SESSION['isframed'] = 1;
 
 include "common/config.inc.php";
 $CONFIG = include($config_dir.'/rslight.inc.php');
@@ -13,7 +12,7 @@ if (isset($_REQUEST['menu'])) {
     $default_menu=$_REQUEST['menu'];
 }
 
-die("header redirect");
+//die("die index.php header redirect");
 header('Location: '.$CONFIG['default_content']);
 
 ?>
