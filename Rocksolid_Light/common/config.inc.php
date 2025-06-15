18f8d207a6d8447e6bc31de581c010eba5d56ad3
go-while <go-while@users.noreply.github.com>
2025-06-15 01:11:19 +0100

    Update config.inc.php

--- Rocksolid_Light/common/config.inc.php
+++ Rocksolid_Light/common/config.inc.php
@@ -1,15 +1,28 @@
 <?php
+/*
+$config_dir = "/etc/rslight/"; // TODO FIXME LATER: NEEDS /!? REMOVE HARDCODED PATH AND REPLACE WITH PLACEHOLDER AFTER TESTING <config_dir>
+$spooldir = "/var/spool/rslight"; // TODO FIXME LATER: NEEDS NO /!? REMOVE HARDCODED PATH AND REPLACE WITH PLACEHOLDER AFTER TESTING <spooldir>
+$config_file = $config_dir.'rslight.inc.php';
+$CONFIG = include $config_file;
+$title = $CONFIG['title_full']; // TODO WHY HERE?
+*/
+//if(isset($config_name)) {die("common/config.inc.php loading alternate config file: $config_file<br>\n");}
 
+/* OLD config.inc.php KEEP FOR REFERENCE */
 /* Location of configuration and spool */
-$config_dir = "<config_dir>";
-$spooldir = "<spooldir>";
+
+$config_dir = "/etc/rslight/"; // TODO FIXME LATER: NEEDS /!? REMOVE HARDCODED PATH AND REPLACE WITH PLACEHOLDER AFTER TESTING <config_dir>
+$spooldir = "/var/spool/rslight"; // TODO FIXME LATER: NEEDS NO /!? REMOVE HARDCODED PATH AND REPLACE WITH PLACEHOLDER AFTER TESTING <spooldir>
 
 if(isset($config_name) && file_exists($config_dir.$config_name.'.inc.php')) {
   $config_file = $config_dir.$config_name.'.inc.php';
+  die("alternate config file: $config_file\n");
+
 } else {
   $config_file = $config_dir.'rslight.inc.php';
+  echo "Using default config file: $config_file\n";
 }
-/* Include main config file for rslight */
+// Include main config file for rslight
 $CONFIG = include $config_file;
 
 $title = $CONFIG['title_full'];
@@ -17,4 +30,5 @@ $title = $CONFIG['title_full'];
 if(!file_exists($config_dir.'/DEBUG')) {
   ini_set('error_reporting', E_ERROR );
 }
+
 ?>
