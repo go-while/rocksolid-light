d00c75bc2f372d2847d2d24e57f12f5ecfa787f4
go-while <go-while@users.noreply.github.com>
2025-06-15 05:05:16 +0100

    Merge $alphabet

--- Rocksolid_Light/common/alphabet.inc.php
+++ Rocksolid_Light/common/alphabet.inc.php
@@ -1,21 +1,5 @@
 <?php
-
-// Include security functions for data integrity
-require_once(__DIR__ . '/../rocksolid/lib/security.inc.php');
-
-// Add security headers
-add_security_headers();
-
-    $alphabet = array('K', 'g', 'A', 'D', 'R', 'V', 's', 'L', 'Q', 'w');
-    $alphabetsForNumbers = array(
-        array('K', 'g', 'A', 'D', 'R', 'V', 's', 'L', 'Q', 'w'),
-        array('M', 'R', 'o', 'F', 'd', 'X', 'z', 'a', 'K', 'L'),
-        array('H', 'Q', 'O', 'T', 'A', 'B', 'C', 'D', 'e', 'F'),
-        array('T', 'A', 'p', 'H', 'j', 'k', 'l', 'z', 'x', 'v'),
-        array('f', 'b', 'P', 'q', 'w', 'e', 'K', 'N', 'M', 'V'),
-        array('i', 'c', 'Z', 'x', 'W', 'E', 'g', 'h', 'n', 'm'),
-        array('O', 'd', 'q', 'a', 'Z', 'X', 'C', 'b', 't', 'g'),
-        array('p', 'E', 'J', 'k', 'L', 'A', 'S', 'Q', 'W', 'T'),
-        array('f', 'W', 'C', 'G', 'j', 'I', 'O', 'P', 'Q', 'D'),
-        array('A', 'g', 'n', 'm', 'd', 'w', 'u', 'y', 'x', 'r')
-    );
+$backtrace = debug_backtrace();
+$parent = isset($backtrace[0]['file']) ? $backtrace[0]['file'] : 'Direct execution';
+echo "[common/alphabet.inc.php (STUB) included by: " . basename($parent) . "]<br>\n";
+?>
