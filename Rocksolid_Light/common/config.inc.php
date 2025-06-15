a9d1948645f2e32cbe2c11d6e1c7a291bd14e9da
go-while <go-while@users.noreply.github.com>
2025-06-15 01:42:18 +0100

    Update config.inc.php

--- Rocksolid_Light/common/config.inc.php
+++ Rocksolid_Light/common/config.inc.php
@@ -1,9 +1,12 @@
 <?php
 ini_set('error_reporting', 0); // show no errors at all, only log them
+$backtrace = debug_backtrace();
+$parent = isset($backtrace[0]['file']) ? $backtrace[0]['file'] : 'Direct execution';
+echo "[common/config.inc.php included by: " . basename($parent) . "]<br>\n";
 
 $config_dir = "/etc/rslight/"; // TODO FIXME LATER: NEEDS /!? REMOVE HARDCODED PATH AND REPLACE WITH PLACEHOLDER AFTER TESTING <config_dir>
 $spooldir = "/var/spool/rslight"; // TODO FIXME LATER: NEEDS NO /!? REMOVE HARDCODED PATH AND REPLACE WITH PLACEHOLDER AFTER TESTING <spooldir>
 $config_file = $config_dir.'rslight.inc.php';
-$CONFIG = include $config_file;
+$CONFIG = require_once($config_file);
 $title = $CONFIG['title_full']; // TODO WHY HERE?
 ?>
